50bf74bc918b06214a391d01228a768b819a6881
Giorgio Previtera <gprevitera@equalexperts.com>
2019-03-20 18:02:07 +0000

    Update install script to captureall dependencies

--- install_plugins.sh
+++ install_plugins.sh
@@ -1,14 +1,48 @@
 #!/usr/bin/env bash
 
+set -x
 set -euo pipefail
 
-#https://github.com/vim-scripts/greplace.vim.git \
-#https://github.com/vim-syntastic/syntastic.git \
-#https://github.com/amiorin/vim-project.git \
+# Install neovim and all dependencies
+_dependencies="\
+    neovim \
+    the_silver_searcher \
+    shfmt \
+    shellcheck \
+    ctags \
+    git \
+"
 
+brew install ${_dependencies} || brew upgrade ${_dependencies}
 
-mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/local
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+# Create directory structure
+mkdir -p \
+    ~/.vim/autoload \
+    ~/.vim/bundle \
+    ~/.vim/local
+
+# Install vim plug
+curl -fLSs \
+    --create-dirs \
+    -o ~/.vim/autoload/plug.vim \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-curl -LSso ~/.vim/local/yaml.vim https://raw.githubusercontent.com/vim-scripts/yaml.vim/master/colors/yaml.vim
 
+# Add yaml syntax
+curl -fLSs \
+    --create-dirs \
+    -o ~/.vim/local/yaml.vim \
+    https://raw.githubusercontent.com/vim-scripts/yaml.vim/master/colors/yaml.vim
+
+# Link config for neovim
+ln -fs ~/.vim/vimrc ~/.config/nvim/init.vim
+ln -fs ~/.vim/autoload ~/.config/nvim/autoload
+
+# Install python dependencies
+pip install neovim jedi autopep8
+
+# Install nerd font
+brew tap homebrew/cask-fonts
+brew cask install font-robotomono-nerd-font-mono
+
+# Install all plugins
+nvim +PlugInstall +qall
