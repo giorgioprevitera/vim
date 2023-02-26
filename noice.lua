396f20721e6a7ffc222161ab8f731488d53217b9
Giorgio Previtera <giorgioprevitera@gmail.com>
2023-02-26 17:26:35 +0000

    Send info to mini view

--- noice.lua
+++ noice.lua
@@ -15,6 +15,7 @@ require("noice").setup({
     view = "mini", -- default view for messages
     view_error = "mini", -- view for errors
     view_warn = "mini", -- view for warnings
+    view_info = "mini", -- view for warnings
     -- view_history = "messages", -- view for :messages
     -- view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
   },
